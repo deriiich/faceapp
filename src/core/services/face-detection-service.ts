56454146e7ef6d14e9f747e6782ac4411b15c78a
Derich <deriiich@gmail.com>
2025-04-16 07:22:07 +0800

    Added temporary logic for switching on local and deployed

--- src/core/services/face-detection-service.ts
+++ src/core/services/face-detection-service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { FaceDetectionApiResponse } from '../models/faceDetection.model';
-import { environment } from "@environments/environment"
 @Injectable({
     providedIn: 'root',
 })
 
 export class FaceDetectionService {
-    private readonly apiUrl = environment.apiUrl;
 
-    constructor(private http: HttpClient) { }
+    private readonly apiUrl: string = '';
+
+    constructor(private http: HttpClient) {
+        const isLocal = window.location.hostname === 'localhost';
+        this.apiUrl = isLocal ? '/api/detect' : 'https://faceapi-2snbrq2o3a-ue.a.run.app';
+    }
 
     detectFaces(imageBase64: string): Observable<FaceDetectionApiResponse> {
         const requestBody = {
